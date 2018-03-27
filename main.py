09290b44a013889c3b6cce64d54c367586f58aa2
VERO <vero.mata.0819@gmail.com>
2018-03-26 21:17:24 -0600

    coordenadas

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, abort, request 
 import numpy as np
 import pandas as pd
 from scipy import signal
@@ -8,24 +8,34 @@ import json
 app = Flask(__name__)
 
 
+# def getplanner(coornenadas):
+# 	df = coornenadas.convert_objects(convert_numeric=True)
+# 	xs = df.y 
+# 	data = np.sin(xs)
+# 	peakind = signal.find_peaks_cwt(data, np.arange(0.1,1,0.1))
+# 	xs[peakind]
+# 	print(df.x[peakind])
 
-@app.route('/hello_world', methods=['POST'])
-def hello_world(coornenadas):
-    df = coornenadas.convert_objects(convert_numeric=True)
-    xs = df.y 
-    data = np.sin(xs)
-    peakind = signal.find_peaks_cwt(data, np.arange(0.1,1,0.1))
-    xs[peakind]
-    print(df.x[peakind])
 
-data = json.load(open('datos.json'))
-df = pd.DataFrame.from_dict(data, orient='index')
-coordenadas_x_y = pd.DataFrame({'x':df.x[0],'y':df.y[0]})
-coordenadas_x_y['xy'] = coordenadas_x_y.apply(lambda row: str(row.x)+str(row.y), axis=1)
-get_planner(coordenadas_x_y)
+@app.route('/hello_world',methods=['POST'])
+def hello_world():
+	data = request.json
+	coordenadas_x_y = pd.DataFrame.from_dict(data)
+	coordenadas_x_y['xy'] = coordenadas_x_y.apply(lambda row: str(row.x)+str(row.y), axis=1)
+	df = coordenadas_x_y.convert_objects(convert_numeric=True)
+	xs = df.y 
+	data = np.sin(xs)
+	peakind = signal.find_peaks_cwt(data, np.arange(0.1,1,0.1))
+	return df.x[peakind].to_json()	
+	# if request.headers['Content-Type'] == 'text/plain':
+	# 	return "Text Message: " + request.data
+	# elif request.headers['Content-Type'] == 'application/json':
+	# 	return "JSON Message: " + json.dumps(request.json)
+	# else:
+	# 	return "415 Unsupported Media Type ;)"
 
 
 if __name__ == '__main__':
-  app.run()
+  	app.run()
 
 
