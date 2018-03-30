a4f540512491d57dcd2ba4f6e73dd5d8f2ce496f
VERO <vero.mata.0819@gmail.com>
2018-03-30 13:34:13 -0600

    ok

--- main.py
+++ main.py
@@ -4,42 +4,23 @@ import pandas as pd
 from scipy import signal
 import json
 
-
 app = Flask(__name__)
-
-
-# def getplanner(coornenadas):
-# 	df = coornenadas.convert_objects(convert_numeric=True)
-# 	xs = df.y 
-# 	data = np.sin(xs)
-# 	peakind = signal.find_peaks_cwt(data, np.arange(0.1,1,0.1))
-# 	xs[peakind]
-# 	print(df.x[peakind])
-
 
 @app.route('/hello_world',methods=['GET','POST'])
 def hello_world():
-	if request.method == 'POST':
-		data = request.json
-		coordenadas_x_y = pd.DataFrame.from_dict(data)
-		coordenadas_x_y['xy'] = coordenadas_x_y.apply(lambda row: str(row.x)+str(row.y), axis=1)
-		df = coordenadas_x_y.convert_objects(convert_numeric=True)
-		xs = df.y 
-		data = np.sin(xs)
-		peakind = signal.find_peaks_cwt(data, np.arange(0.1,1,0.1))
-		return df.x[peakind].to_json()	
-		# if request.headers['Content-Type'] == 'text/plain':
-		# 	return "Text Message: " + request.data
-		# elif request.headers['Content-Type'] == 'application/json':
-		# 	return "JSON Message: " + json.dumps(request.json)
-		# else:
-		# 	return "415 Unsupported Media Type ;)"
-	if request.method == 'GET':
-		return "hola..."
-	else:
-		return "nsda de nada"
+  if request.method == 'POST':
+    data = request.json
+    coordenadas_x_y = pd.DataFrame.from_dict(data)
+    coordenadas_x_y['xy'] = coordenadas_x_y.apply(lambda row: str(row.x)+str(row.y), axis=1)
+    df = coordenadas_x_y.convert_objects(convert_numeric=True)
+    xs = df.y 
+    data = np.sin(xs)
+    peakind = signal.find_peaks_cwt(data, np.arange(0.1,1,0.1))
+    return df.x[peakind].to_json()
+  if request.method == 'GET':
+    return "get"
+  else:
+    return "nsda de nada"
 
 if __name__ == '__main__':
-  	app.run()
-
-
+  app.run()
